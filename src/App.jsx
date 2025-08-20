bf536c007b932fafccd740c62a94f46ee4faa95e
Ragul-JustLogix <ragul.m@justlogix.com>
2025-08-20 18:56:00 +0530

    kkkdklkd

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ const App = () => {
   return (
     <div>
       <h1>Welcome to My React App</h1>
+      
+  
       <p>This is a simple React application.</p>
       <p>It is designed to help you get started with React development.</p>
       <p>Feel free to explore and modify the code!</p>
